423239d76b8d70954269af4359b99bf021e5ab2b
zhangchao <zhangxiaochao163@163.com>
2015-05-21 22:54:44 +0800

    初始化项目

---
+++ zhangchao/zhangchao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  zhangchao
+//
+//  Created by 张超 on 15-5-21.
+//  Copyright (c) 2015年 张超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ zhangchao/zhangchao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  zhangchao
+//
+//  Created by 张超 on 15-5-21.
+//  Copyright (c) 2015年 张超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
